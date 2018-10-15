37a79f3b2cb0fad6c9d231493275078a4232e8df
shinp <shinpeiyoshihara@gmail.com>
2018-10-16 08:23:20 +0900

    aa

--- test.ts
+++ test.ts
@@ -2,44 +2,59 @@ import * as SEQ from "sequelize-typescript";
 import * as Models from "./models";
 
 var v = new SEQ.Sequelize("sqlite:./test.db");
+v.addModels([Models.Person, Models.N]);
+
+v.sync({})
+  .then(
+    () => {
+      return Models.Person.create({
+        name: 'waz',
+        birthday: '2000-01-02',
+      })
+        ;
+    }
+  )
+  .then(
+    $p => Models.N.create({
+      n: 144,
+      person_id: $p.id
+    })
+  )
+  .then(
+    $n => Models.N.create({
+      n: 145,
+      person_id: $n.person_id
+    })
+  )
+  .then(
+    $n => Models.N.create({
+      n: 146,
+      person_id: $n.person_id
+    })
+  )
+  .then(
+    $n => {
+
+      var $o = Models.Person.findById(1)
+        .then(
+          $r => {
+            console.dir($r.ns);
+            $r.ns.forEach(
+              $n => console.log(`PID= ${$r.id} CID=${$n.id}`)
+            )
+            console.log()
+          }
+        )
+    }
+  )
 
 
 
-v.addModels([Models.Person,Models.N]);
-v.sync({});
 
 
-
-Models.Person.create({
-  name: 'waz',
-  birthday: '2000-01-02',
-  ns:[
-    {n:44},
-    {n:45},
-  ]
-}).then(
-  $p=>{
-    console.log(`${$p.name}: ${$p.id}`);
-    Models.N.create({
-      n:144,
-      person_id:$p.id
-    });
-  }
-)
-;
 
 
 
 //p.save();
-
-
-
 //Models.N.create({n: 5,person_id:p.id}),
-
-
-
-
-
-
-
 console.log("OK");
